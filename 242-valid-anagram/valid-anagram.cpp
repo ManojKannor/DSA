ea541ba5f37042a7965dbdcceedfdf37da82dbbb
Manoj Kannor <ictmyk2023@gmail.com>
2026-09-26 22:38:58 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (91.86%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        if(s.length() != t.length()) return false;
 
-        if(s == t) {
-            return true;
+       int count_s[26] = {0};
+       int count_t[26] = {0};
+
+       for(int i=0;i<s.length();i++){
+        count_s[s[i] - 'a']++;
+        count_t[t[i] - 'a']++;
+       }
+
+       for(int i=0;i<26;i++){
+        if(count_s[i] != count_t[i]){
+            return false;
         }
-        return false;
+        
+       }
+
+       return true;
     }
 };
